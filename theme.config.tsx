9ca6c45263d0df1f0097c6fdb4a1ca50100fdf2f
Nathan Fiorito <pessoal@nathanfiorito.com.br>
2026-04-13 10:46:19 -0300

    feat: apply frontend design system (Inter font, orange brand, dark mode)

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Personal Finances</span>,
+  logo: (
+    <span style={{ fontWeight: 700, fontSize: '1rem', letterSpacing: '-0.01em' }}>
+      Personal Finances
+    </span>
+  ),
+  primaryHue: 24,
+  primarySaturation: 93,
   project: {
     link: 'https://github.com/nathanfiorito/personal-finances',
   },
